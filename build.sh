a32868f79d2d0402daaf04df421d2950a9c07100
unknown <vs-zhangjiawei@voyah.com.cn>
2026-04-23 10:32:12 +0800

    升级工具链

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 #工具链下载地址
-#https://repo.huaweicloud.com/harmonyos/os/4.1.4-Release/ohos-sdk-windows_linux-public.tar.gz
+#https://repo.huaweicloud.com/harmonyos/os/5.1.0-Release/ohos-sdk-windows_linux-public.tar.gz
+#clang下载地址
+#https://repo.huaweicloud.com/harmonyos/compiler/clang/15.0.4-283a25/linux/clang_linux-x86_64-283a25-20260403.tar.gz
 BUILD_DIR=$(pwd)/build/
 rm -rf ${BUILD_DIR}
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
 
-cmake -DCMAKE_TOOLCHAIN_FILE="/opt/native-linux-x64-4.1.10.5-Release/build/cmake/ohos.toolchain.cmake"  ..
+#cmake ..
+#cmake -DCMAKE_TOOLCHAIN_FILE="CrossCompile.cmake"  ..
+#cmake -DCMAKE_TOOLCHAIN_FILE="/opt/native-linux-x64-4.1.10.5-Release/build/cmake/ohos.toolchain.cmake"  ..
+cmake -DCMAKE_TOOLCHAIN_FILE="/opt/native-linux-x64-5.1.0.107-Release/build/cmake/ohos.toolchain.cmake" ..
 make
