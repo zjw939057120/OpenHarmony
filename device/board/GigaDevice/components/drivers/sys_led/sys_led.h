9a9bf58571629112c71d9ffd04ee91204c945fb0
zjw939057120 <939057120@qq.com>
2026-08-15 12:20:09 +0800

    GD32F470ZI

--- device/board/GigaDevice/components/drivers/sys_led/sys_led.h
+++ device/board/GigaDevice/components/drivers/sys_led/sys_led.h
@@ -26,7 +26,7 @@ extern "C" {
 #endif
 
 /* LED 定义 指示灯引脚 */
-#if (defined GD32F470VI) || (defined GD32F407VE)
+#if (defined GD32F470VI) || (defined GD32F470ZI)
 #define LED_PIN                         GPIO_PIN_8
 #define LED_PORT                        GPIOA
 #define LED_CLK                         RCU_GPIOA
